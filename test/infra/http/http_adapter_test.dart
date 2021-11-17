fdca61093b706476f962bb971e978051a700abf2
Vinicius Morais <viniciusmoraisxd@gmail.com>
2021-11-17 16:22:25 -0300

    ensure HttpAdapter calls post without Body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -22,7 +22,9 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
-    await client.post(url, headers: headers, body: jsonEncode(body));
+
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await client.post(url, headers: headers, body: jsonBody);
   }
 }
 
@@ -49,6 +51,22 @@ void main() {
             'accept': 'application/json'
           },
           body: '{"any_key":"any_value"}',
+        ),
+      );
+    });
+
+    test('Should call post without body', () async {
+      client = ClientSpy();
+      sut = HttpAdapter(client);
+
+      url = faker.internet.httpUrl();
+
+      await sut.request(url: url, method: 'post');
+
+      verify(
+        client.post(
+          any,
+          headers: anyNamed('headers'),
         ),
       );
     });
