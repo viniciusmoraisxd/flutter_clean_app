228d06bca7a4483836728fbc2f182534ad4ba4e7
Vinicius Morais <viniciusmoraisxd@gmail.com>
2021-11-20 16:25:43 -0300

    Ensure login page presents no error if password is valid

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -44,17 +44,23 @@ class LoginPage extends StatelessWidget {
                         }),
                     Padding(
                       padding: const EdgeInsets.only(top: 8, bottom: 32.0),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Senha',
-                          icon: Icon(
-                            Icons.lock,
-                            color: Theme.of(context).primaryColorLight,
-                          ),
-                        ),
-                        obscureText: true,
-                        onChanged: presenter.validatePassword,
-                      ),
+                      child: StreamBuilder<String>(
+                          stream: presenter.passwordErrorStream,
+                          builder: (context, snapshot) {
+                            return TextFormField(
+                              decoration: InputDecoration(
+                                  labelText: 'Senha',
+                                  icon: Icon(
+                                    Icons.lock,
+                                    color: Theme.of(context).primaryColorLight,
+                                  ),
+                                  errorText: snapshot.data?.isEmpty == true
+                                      ? null
+                                      : snapshot.data),
+                              obscureText: true,
+                              onChanged: presenter.validatePassword,
+                            );
+                          }),
                     ),
                     RaisedButton(
                       onPressed: null,
--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -11,19 +11,24 @@ class LoginPresenterSpy extends Mock implements LoginPresenter {}
 void main() {
   LoginPresenterSpy presenter;
   StreamController<String> emailErrorController;
+  StreamController<String> passwordErrorController;
 
   Future<void> loadPage(WidgetTester tester) async {
     presenter = LoginPresenterSpy();
     emailErrorController = StreamController<String>();
+    passwordErrorController = StreamController<String>();
 
     when(presenter.emailErrorStream)
         .thenAnswer((_) => emailErrorController.stream);
+    when(presenter.passwordErrorStream)
+        .thenAnswer((_) => passwordErrorController.stream);
     final loginPage = MaterialApp(home: LoginPage(presenter));
     await tester.pumpWidget(loginPage);
   }
 
   tearDown(() {
     emailErrorController.close();
+    passwordErrorController.close();
   });
 
   testWidgets("Should load with correct initial state",
@@ -93,6 +98,46 @@ void main() {
     expect(
         find.descendant(
             of: find.bySemanticsLabel("E-mail"), matching: find.byType(Text)),
+        findsOneWidget);
+  });
+
+
+
+
+  // password tests
+
+  testWidgets("Should present error if password is invalid",
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    passwordErrorController.add('any_error');
+    await tester.pump();
+
+    expect(find.text('any_error'), findsOneWidget);
+  });
+  testWidgets("Should present no error if password is valid",
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    passwordErrorController.add(null);
+    await tester.pump();
+
+    expect(
+        find.descendant(
+            of: find.bySemanticsLabel("Senha"), matching: find.byType(Text)),
+        findsOneWidget);
+  });
+
+  testWidgets("Should present no error if password is valid",
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    passwordErrorController.add("");
+    await tester.pump();
+
+    expect(
+        find.descendant(
+            of: find.bySemanticsLabel("Senha"), matching: find.byType(Text)),
         findsOneWidget);
   });
 }
