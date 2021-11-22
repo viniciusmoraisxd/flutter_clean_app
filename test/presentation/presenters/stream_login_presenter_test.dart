38e2d709f185592ee4454de0d10dcf24a306730f
Vinicius Morais <viniciusmoraisxd@gmail.com>
2021-11-22 19:40:35 -0300

    ensure StreamLoginPresenter calls validation on email changed

---
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -0,0 +1,31 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:meta/meta.dart';
+
+abstract class Validation {
+  String validate({@required String field, @required String value});
+}
+
+class StreamLoginPresenter {
+  final Validation validation;
+
+  StreamLoginPresenter({@required this.validation});
+  void validateEmail(String email) {
+    validation.validate(field: 'email', value: email);
+  }
+}
+
+class ValidationSpy extends Mock implements Validation {}
+
+void main() {
+  test('Should call validation with correct email', () {
+    final validation = ValidationSpy();
+    final sut = StreamLoginPresenter(validation: validation);
+
+    final email = faker.internet.email();
+    sut.validateEmail(email);
+
+    verify(validation.validate(field: 'email', value: email)).called(1);
+  });
+}
