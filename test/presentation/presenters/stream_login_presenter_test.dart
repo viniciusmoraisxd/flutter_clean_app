ba87afbae9645aa53c31d74290defa0323f89372
Vinicius Morais <viniciusmoraisxd@gmail.com>
2021-11-23 22:13:16 -0300

    ensure StreamLoginPresenter emits null if email is valid

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -24,6 +24,8 @@ void main() {
     validation = ValidationSpy();
     sut = StreamLoginPresenter(validation: validation);
     email = faker.internet.email();
+
+    mockValidation(); //success validation
   });
 
   test('Should call validation with correct email', () {
@@ -39,9 +41,19 @@ void main() {
     sut.emailErrorStream
         .listen(expectAsync1((error) => expect(error, 'error')));
 
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
 
-    sut.isFormValidStream.listen(expectAsync1((isValid) => expect(isValid, false)));
+    sut.validateEmail(email);
+  });
 
+  test('Should emit null if validation succeds', () {
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateEmail(email);
     sut.validateEmail(email);
   });
 }
