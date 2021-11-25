7c958b722bc018594a5d296faf56505535837438
Vinicius Morais <viniciusmoraisxd@gmail.com>
2021-11-24 22:21:41 -0300

    ensure RequiredFieldValidation work as expected on empty and not empty fields

---
+++ test/validation/validators/required_field_validation_test.dart
@@ -0,0 +1,34 @@
+import 'package:test/test.dart';
+
+abstract class FieldValidation {
+  String get field;
+  String validate(String value);
+}
+
+class RequiredFieldValidation implements FieldValidation {
+  final String field;
+  RequiredFieldValidation(this.field);
+
+  @override
+  String validate(String value) {
+    return value.isEmpty ? 'Campo obrigatório' : null;
+  }
+}
+
+void main() {
+  test('Should return null if value is not empty', () {
+    final sut = RequiredFieldValidation('any_field');
+
+    final error = sut.validate('any_value');
+
+    expect(error, null);
+  });
+
+  test('Should return error if value is empty', () {
+    final sut = RequiredFieldValidation('any_field');
+
+    final error = sut.validate('');
+
+    expect(error, 'Campo obrigatório');
+  });
+}
