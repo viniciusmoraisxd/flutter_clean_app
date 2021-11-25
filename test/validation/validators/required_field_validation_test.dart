ac795ff71253fdbfa4a6a31c71fcfcf4af0177f9
Vinicius Morais <viniciusmoraisxd@gmail.com>
2021-11-24 22:27:45 -0300

    ensure RequiredFieldValidation work as expected when field is null

--- test/validation/validators/required_field_validation_test.dart
+++ test/validation/validators/required_field_validation_test.dart
@@ -11,7 +11,7 @@ class RequiredFieldValidation implements FieldValidation {
 
   @override
   String validate(String value) {
-    return value.isEmpty ? 'Campo obrigatório' : null;
+    return value?.isNotEmpty == true ? null : 'Campo obrigatório';
   }
 }
 
@@ -21,12 +21,14 @@ void main() {
     sut = RequiredFieldValidation('any_field');
   });
   test('Should return null if value is not empty', () {
-
     expect(sut.validate('any_value'), null);
   });
 
   test('Should return error if value is empty', () {
-
     expect(sut.validate(''), 'Campo obrigatório');
+  });
+
+  test('Should return error if value is null', () {
+    expect(sut.validate(null), 'Campo obrigatório');
   });
 }
