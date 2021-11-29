b720b42409b15bedf3e742a63f743c930e2f1704
Vinicius Morais <viniciusmoraisxd@gmail.com>
2021-11-29 20:18:04 -0300

    Add validation builder

---
+++ lib/main/builders/validation_builder.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_clean_app/validation/protocols/protocols.dart';
+import 'package:flutter_clean_app/validation/validators/validators.dart';
+
+class ValidationBuilder {
+  static ValidationBuilder _instance;
+  String fieldName;
+  List<FieldValidation> validations = [];
+
+  static ValidationBuilder field(String fieldName) {
+    _instance = ValidationBuilder();
+    _instance.fieldName = fieldName;
+
+    return _instance;
+  }
+
+  ValidationBuilder required() {
+    validations.add(RequiredFieldValidation(fieldName));
+    return this;
+  }
+
+  ValidationBuilder email() {
+    validations.add(EmailValidation(fieldName));
+    return this;
+  }
+
+  List<FieldValidation> build() => validations;
+}
--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,6 +1,5 @@
+import 'package:flutter_clean_app/main/builders/builders.dart';
 import 'package:flutter_clean_app/validation/protocols/protocols.dart';
-import 'package:flutter_clean_app/validation/validators/required_field_validation.dart';
-import 'package:flutter_clean_app/validation/validators/validation_composite.dart';
 import 'package:flutter_clean_app/validation/validators/validators.dart';
 
 ValidationComposite makeValidationComposite() =>
@@ -8,8 +7,7 @@ ValidationComposite makeValidationComposite() =>
 
 List<FieldValidation> makeLoginValidations() {
   return [
-    RequiredFieldValidation('email'),
-    EmailValidation('email'),
-    RequiredFieldValidation('password')
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().build(),
   ];
 }
