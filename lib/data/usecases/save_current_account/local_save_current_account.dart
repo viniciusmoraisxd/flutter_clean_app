75c71a66cde147ff11585f7a6d2f62776bec2f9d
Vinicius Morais <viniciusmoraisxd@gmail.com>
2021-12-04 12:37:48 -0300

    move classes to production files

---
+++ lib/data/usecases/save_current_account/local_save_current_account.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_clean_app/data/cache/cache.dart';
+import 'package:flutter_clean_app/domain/entities/entities.dart';
+import 'package:flutter_clean_app/domain/helpers/helpers.dart';
+import 'package:flutter_clean_app/domain/usecases/usecases.dart';
+import 'package:meta/meta.dart';
+
+class LocalSaveCurrentAccount implements SaveCurrentAccount {
+  final SaveSecureCacheStorage saveSecureCacheStorage;
+
+  LocalSaveCurrentAccount({@required this.saveSecureCacheStorage});
+  @override
+  Future<void> save(AccountEntity account) async {
+    try {
+      await saveSecureCacheStorage.saveSecure(
+          key: 'token', value: account.token);
+    } catch (e) {
+      throw DomainError.unexpected;
+    }
+  }
+}
--- test/data/usecases/save_current_account/local_save_current_account_test.dart
+++ test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -1,29 +1,10 @@
 import 'package:faker/faker.dart';
+import 'package:flutter_clean_app/data/cache/cache.dart';
+import 'package:flutter_clean_app/data/usecases/usecases.dart';
 import 'package:flutter_clean_app/domain/entities/account_entity.dart';
 import 'package:flutter_clean_app/domain/helpers/helpers.dart';
-import 'package:flutter_clean_app/domain/usecases/usecases.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
-
-class LocalSaveCurrentAccount implements SaveCurrentAccount {
-  final SaveSecureCacheStorage saveSecureCacheStorage;
-
-  LocalSaveCurrentAccount({@required this.saveSecureCacheStorage});
-  @override
-  Future<void> save(AccountEntity account) async {
-    try {
-      await saveSecureCacheStorage.saveSecure(
-          key: 'token', value: account.token);
-    } catch (e) {
-      throw DomainError.unexpected;
-    }
-  }
-}
-
-abstract class SaveSecureCacheStorage {
-  Future<void> saveSecure({@required String key, @required String value});
-}
 
 class SaveSecureCacheStorageSpy extends Mock implements SaveSecureCacheStorage {
 }
