96e9651f84eda345d5477a393704c388fbe14244
Vinicius Morais <viniciusmoraisxd@gmail.com>
2021-12-04 14:25:20 -0300

    move localstorageadapter to its own file

---
+++ lib/infra/cache/local_storage_adapter.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_clean_app/data/cache/cache.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:meta/meta.dart';
+
+class LocalStorageAdapter implements SaveSecureCacheStorage {
+  final FlutterSecureStorage secureStorage;
+
+  LocalStorageAdapter({@required this.secureStorage});
+
+  @override
+  Future<void> saveSecure(
+      {@required String key, @required String value}) async {
+    await secureStorage.write(key: key, value: value);
+  }
+}
--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -1,22 +1,9 @@
 import 'package:faker/faker.dart';
-import 'package:flutter_clean_app/data/cache/cache.dart';
-import 'package:flutter_clean_app/domain/helpers/helpers.dart';
+import 'package:flutter_clean_app/infra/cache/cache.dart';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
-
-class LocalStorageAdapter implements SaveSecureCacheStorage {
-  final FlutterSecureStorage secureStorage;
-
-  LocalStorageAdapter({@required this.secureStorage});
-
-  @override
-  Future<void> saveSecure(
-      {@required String key, @required String value}) async {
-    await secureStorage.write(key: key, value: value);
-  }
-}
 
 class FlutterSecureStorageSpy extends Mock implements FlutterSecureStorage {}
 
@@ -38,7 +25,7 @@ void main() {
 
     verify(secureStorage.write(key: key, value: value));
   });
-  test('Should throw an Unexpected error if save secure throws', () async {
+  test('Should throws if save secure throws', () async {
     when(sut.saveSecure(key: anyNamed('key'), value: anyNamed('value')))
         .thenThrow(Exception());
 
