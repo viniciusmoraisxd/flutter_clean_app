520085a6bf430fa568d678121ec9c2a4245f3714
Vinicius Morais <viniciusmoraisxd@gmail.com>
2021-12-21 15:35:40 -0300

    Add AddAccount usecase on Domain Layer

---
+++ lib/domain/usecases/add_account.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+import 'package:flutter_clean_app/domain/entities/account_entity.dart';
+
+abstract class AddAccount {
+  Future<AccountEntity> add();
+}
+
+class AddAccountParams extends Equatable {
+  final String name;
+  final String email;
+  final String password;
+  final String passwordConfirmation;
+
+  AddAccountParams(
+      {@required this.name,
+      @required this.email,
+      @required this.password,
+      @required this.passwordConfirmation});
+
+  @override
+  List get props => [name, email, password, passwordConfirmation];
+}
