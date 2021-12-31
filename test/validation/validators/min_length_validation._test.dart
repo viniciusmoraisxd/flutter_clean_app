26d61bbd5d8a55ad75c4ff281433f36d079c2d30
Vinicius Morais <viniciusmoraisxd@gmail.com>
2021-12-31 14:47:13 -0300

    Ensure minLengthValition returns appropriate errors

---
+++ test/validation/validators/min_length_validation._test.dart
@@ -0,0 +1,41 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_clean_app/presentation/protocols/protocols.dart';
+import 'package:flutter_clean_app/validation/protocols/protocols.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+class MinLengthValidation implements FieldValidation {
+  final String field;
+  final int minLength;
+
+  MinLengthValidation({@required this.field, @required this.minLength});
+
+  @override
+  ValidationError validate(String value) {
+    // final isValid = value?.isNotEmpty != true || value.length > 3;
+
+    // return isValid ? null : ValidationError.invalidField;
+    return ValidationError.invalidField;
+  }
+}
+
+void main() {
+  MinLengthValidation sut;
+
+  setUp(() {
+    sut = MinLengthValidation(field: 'any_field', minLength: 5);
+  });
+
+  test('Should return error if value is empty', () {
+    expect(sut.validate(''), ValidationError.invalidField);
+  });
+
+  test('Should return error if value is null', () {
+    expect(sut.validate(null), ValidationError.invalidField);
+  });
+
+  test('Should return an error if value less than minLength', () {
+    expect(sut.validate(faker.randomGenerator.string(4, min: 1)),
+        ValidationError.invalidField);
+  });
+}
