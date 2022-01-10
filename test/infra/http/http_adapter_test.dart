01ad594132ee9e89d7132e13b4b9aa9db69deded
Vinicius Morais <viniciusmoraisxd@gmail.com>
2022-01-10 12:11:50 -0300

    ensure HttpAdapter calls get with correct values

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -154,4 +154,32 @@ void main() {
       expect(future, throwsA(HttpError.serverError));
     });
   });
+
+  group('Get Tests', () {
+    PostExpectation mockRequest() =>
+        when(client.get(any, headers: anyNamed('headers')));
+
+    void mockResponse(int statusCode,
+        {String body = '{"any_key":"any_value"}'}) {
+      mockRequest().thenAnswer((_) async => Response(body, statusCode));
+    }
+
+    setUp(() {
+      mockResponse(200);
+    });
+
+    test('Should call GET with correct values', () async {
+      await sut.request(url: url, method: 'get');
+
+      verify(
+        client.get(
+          url,
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+          },
+        ),
+      );
+    });
+  });
 }
