7df1670a22d5049a4fc60379f5761890ae8b5cb6
Vinicius Morais <viniciusmoraisxd@gmail.com>
2022-01-24 11:37:59 -0300

    Improve SurveysPage layout

--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -33,14 +33,26 @@ class SurveysPage extends StatelessWidget {
               stream: presenter.surveysStream,
               builder: (context, snapshot) {
                 if (snapshot.hasError) {
-                  return Column(
-                    children: [
-                      Text(snapshot.error),
-                      RaisedButton(
-                        child: Text(R.strings.reload),
-                        onPressed: presenter.loadData,
-                      )
-                    ],
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          snapshot.error,
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        RaisedButton(
+                          child: Text(R.strings.reload),
+                          onPressed: presenter.loadData,
+                        )
+                      ],
+                    ),
                   );
                 }
 
