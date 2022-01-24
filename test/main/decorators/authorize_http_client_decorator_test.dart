eb6b83798ac426f7393064d0098f3126a9093058
Vinicius Morais <viniciusmoraisxd@gmail.com>
2022-01-24 13:36:35 -0300

    Add params to request

--- test/main/decorators/authorize_http_client_decorator_test.dart
+++ test/main/decorators/authorize_http_client_decorator_test.dart
@@ -1,3 +1,4 @@
+import 'package:faker/faker.dart';
 import 'package:flutter_clean_app/data/cache/cache.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
@@ -7,7 +8,12 @@ class AuthorizeHttpClientDecorator {
   final FetchSecureCacheStorage fetchSecureCacheStorage;
 
   AuthorizeHttpClientDecorator({@required this.fetchSecureCacheStorage});
-  Future<void> request() async {
+  Future<void> request({
+    @required String url,
+    @required method,
+    Map body,
+    Map headers,
+  }) async {
     await fetchSecureCacheStorage.fetchSecure('token');
   }
 }
@@ -18,16 +24,22 @@ class FetchSecureCacheStorageSpy extends Mock
 void main() {
   FetchSecureCacheStorageSpy fetchSecureCacheStorageSpy;
   AuthorizeHttpClientDecorator sut;
+  String url;
+  String method;
+  Map body;
 
   setUp(() {
     fetchSecureCacheStorageSpy = FetchSecureCacheStorageSpy();
     sut = AuthorizeHttpClientDecorator(
         fetchSecureCacheStorage: fetchSecureCacheStorageSpy);
+
+    url = faker.internet.httpUrl();
+    method = faker.randomGenerator.string(10);
+    body = {'any_key': 'any_value'};
   });
 
   test('Should call FetchSecureCacheStorage with correct key', () async {
-    await sut.request();
-
+    await sut.request(url: url, method: method, body: body);
     verify(fetchSecureCacheStorageSpy.fetchSecure('token')).called(1);
   });
 }
