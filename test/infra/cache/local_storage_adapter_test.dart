02dcfc2493fc7f6ba1969dfa26d90d4d79ed7f95
Vinicius Morais <viniciusmoraisxd@gmail.com>
2022-01-28 20:02:04 -0300

    Move production code in LocalStorageAdapter test to its own file

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -1,19 +1,8 @@
 import 'package:faker/faker.dart';
+import 'package:flutter_clean_app/infra/cache/cache.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:localstorage/localstorage.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
-
-class LocalStorageAdapter {
-  final LocalStorage localStorage;
-
-  LocalStorageAdapter({@required this.localStorage});
-
-  Future<void> save({@required String key, @required dynamic value}) async {
-    await localStorage.deleteItem(key);
-    await localStorage.setItem(key, value);
-  }
-}
 
 class LocalStorageSpy extends Mock implements LocalStorage {}
 
