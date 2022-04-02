c6e064e623de0473ca116f9e083d117c52b1bb0f
Vinicius Morais <viniciusmoraisxd@gmail.com>
2022-04-02 11:01:26 -0300

    Ensure Composite call load on remote error

--- test/main/composites/remote_load_surveys_with_local_fallback_test.dart
+++ test/main/composites/remote_load_surveys_with_local_fallback_test.dart
@@ -17,11 +17,17 @@ class RemoteLoadSurveysWithLocalFallback implements LoadSurveys {
   });
 
   Future<List<SurveyEntity>> load() async {
-    final surveys = await remote.load();
-
-    await local.save(surveys);
-
-    return surveys;
+    try {
+      final surveys = await remote.load();
+      await local.save(surveys);
+      return surveys;
+    } catch (e) {
+      if (e == DomainError.accessDenied) {
+        rethrow;
+      }
+      await local.validate();
+      await local.load();
+    }
   }
 }
 
@@ -85,5 +91,14 @@ void main() {
     final future = sut.load();
 
     expect(future, throwsA(DomainError.accessDenied));
+  });
+
+  test('Should call local fetch on remote error', () async {
+    mockRemoteLoadError(DomainError.unexpected);
+
+    await sut.load();
+
+    verify(local.validate()).called(1);
+    verify(local.load()).called(1);
   });
 }
