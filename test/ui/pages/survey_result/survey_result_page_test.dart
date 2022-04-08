8c35daf0c3582cfa48370e2ce54c6ccbe4830a4d
Vinicius Morais <viniciusmoraisxd@gmail.com>
2022-04-08 11:19:08 -0300

    ensure SurveyResult calls LoadSurveyResult on reload

--- test/ui/pages/survey_result/survey_result_page_test.dart
+++ test/ui/pages/survey_result/survey_result_page_test.dart
@@ -94,4 +94,15 @@ void main() {
         findsOneWidget);
     expect(find.text('Recarregar'), findsOneWidget);
   });
+
+  testWidgets('Should call LoadSurveyResult on reload button click',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    surveyResultController.addError(UIError.unexpected.description);
+    await tester.pump();
+    await tester.tap(find.text('Recarregar'));
+
+    verify(presenter.loadData()).called(2);
+  });
 }
